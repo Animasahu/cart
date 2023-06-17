dc5445142c11636dd7203b27e0ca47e1965d8c30
Anima Sahu <animasahu2009@gmail.com>
2023-06-17 13:35:11 +0530

    add props to cart item

--- src/CardItem.js
+++ src/CardItem.js
@@ -1,17 +1,7 @@
 import React from 'react';
 
 class CardItem extends React.Component{
-    constructor(){
-        super();
-        this.state ={
-            price:999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-       // this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
-
+    
     increaseQuantity = () =>{
         //this.state.qty += 1;
         //console.log('this', this.state);
@@ -41,9 +31,11 @@ class CardItem extends React.Component{
         });
     }
     render(){
-        const {price, title , qty} = this.state;
+        console.log('this.props', this.props);
+        const {price, title , qty} = this.props.product;
         return(
             <div className="cart-item">
+                {this.props.jsx}
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,50 @@ import React from 'react';
 import CardItem from './CardItem';
 
 class Cart extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            products: [
+                {
+                    price:999,
+                    title: 'Watch',
+                    qty: 1,
+                    img: '',
+                    id: 1
+                },
+                {
+                    price:999,
+                    title: 'Mobile Phone',
+                    qty: 10,
+                    img: '',
+                    id: 2
+                },
+                {
+                    price:999,
+                    title: 'Laptop',
+                    qty: 4,
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+       // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+
    render(){
+    const { products } = this.state;
     return (
         <div className="cart">
-            <CardItem />
-            <CardItem />
-            <CardItem />
+            {products.map((product) => {
+                return (
+                     <CardItem 
+                        product={product} 
+                        key={product.id} 
+        
+                       />
+                )
+            })}
+
         </div>
     );
    }
