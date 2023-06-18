411836059ab03bf6a0b692c3a09d12a956d0f65a
Anima Sahu <animasahu2009@gmail.com>
2023-06-18 11:29:29 +0530

    Delete product from cart

--- src/Cart.js
+++ src/Cart.js
@@ -32,6 +32,43 @@ class Cart extends React.Component{
        // this.increaseQuantity = this.increaseQuantity.bind(this);
     }
 
+    handleIncreaseQuantity = (product) => {
+        console.log('Heyy please inc the qty of', product);
+        const { products }=this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products: products
+        })
+    }
+
+    handleDecreaseQuantity = (product) => {
+        console.log('Heyy please inc the qty of', product);
+        const { products }=this.state;
+        const index = products.indexOf(product);
+
+        if(products[index].qty === 0){
+            return;
+        }
+        products[index].qty -= 1;
+
+        this.setState({
+            products: products
+        })
+    }
+
+    handleDeleteProduct = (id) => {
+        const { products } = this.state;
+
+        const items = products.filter((item) => item.id !== id); // [{}]
+
+        this.setState({
+            products: items
+        })
+    }
+
    render(){
     const { products } = this.state;
     return (
@@ -41,6 +78,9 @@ class Cart extends React.Component{
                      <CardItem 
                         product={product} 
                         key={product.id} 
+                        onIncreaseQuantity = {this.handleIncreaseQuantity}
+                        onDecreaseQuantity = {this.handleDecreaseQuantity}
+                        onDeleteProduct = {this.handleDeleteProduct}
         
                        />
                 )
